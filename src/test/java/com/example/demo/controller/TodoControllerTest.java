35b53c5ab9901c6fd5cdff893a7cd46e8e4fdc21
hero <ghnm89@gmail.com>
2022-12-18 13:56:50 +0900

    :white_check_mark: todo 수정, 삭제 테스트 코드 작성

--- src/test/java/com/example/demo/controller/TodoControllerTest.java
+++ src/test/java/com/example/demo/controller/TodoControllerTest.java
@@ -4,16 +4,19 @@ import com.example.demo.dto.TodoDTO;
 import com.example.demo.entity.TodoEntity;
 import com.example.demo.repository.TodoRepository;
 import com.example.demo.service.TodoService;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class TodoControllerTest {
@@ -23,7 +26,7 @@ class TodoControllerTest {
     @Autowired
     private TodoRepository todoRepository;
 
-    private static final String userid = "user";
+    private static final String userid = "temporary-user";
 
     @Test
     @DisplayName("Todo 생성 테스트")
@@ -69,9 +72,38 @@ class TodoControllerTest {
     @Test
     @DisplayName("Todo 수정 테스트")
     void updateTodo() {
+        TodoEntity beforeEntity = todoRepository.save(TodoEntity.builder()
+                .id("123")
+                .userId(userid)
+                .title("title 1")
+                .done(true)
+                .build());
+        TodoEntity afterEntity = TodoEntity.builder()
+                .id(beforeEntity.getId())
+                .userId(userid)
+                .title("title 2")
+                .done(false)
+                .build();
+
+        TodoDTO todoDTO = todoService.updateTodo(afterEntity);
+
+        assertEquals(todoDTO.getId(), afterEntity.getId());
+        assertEquals(todoDTO.getTitle(), afterEntity.getTitle());
+        assertEquals(todoDTO.getDone(), afterEntity.getDone());
     }
 
     @Test
+    @DisplayName("Todo 삭제 테스트")
     void deleteTodo() {
+        TodoEntity entity = todoRepository.save(TodoEntity.builder()
+                        .id("did")
+                        .userId(userid)
+                        .title("title 133")
+                        .done(false)
+                .build());
+
+        Boolean isDelete = todoService.deleteTodo(entity.getId());
+
+        assertTrue(isDelete);
     }
 }
